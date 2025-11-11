e7feffa2d68b212d2ed53f072a9d2bfd81ffb7bd
colbypapadakis <101018931+CJPAPA2004@users.noreply.github.com>
2025-11-11 12:31:34 -0600

    chore: remove progress chart from dashboard

--- src/components/DashboardScreen.tsx
+++ src/components/DashboardScreen.tsx
@@ -1,6 +1,6 @@
 import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
 import { Button } from "./ui/button";
-import { Plus, TrendingUp, Target, Activity } from "lucide-react";
+import { Plus, Target, Activity } from "lucide-react";
 import { ApiTestComponent } from "./ApiTestComponent";
 
 interface DashboardScreenProps {
@@ -125,48 +125,6 @@ export function DashboardScreen({ onNavigate, userData }: DashboardScreenProps) 
             </CardContent>
           </Card>
         </div>
-
-        {/* Progress Chart */}
-        <Card className="border-2 border-gray-800">
-          <CardHeader>
-            <CardTitle>Progress Over Time</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <div className="border-2 border-gray-400 p-4 h-64 overflow-hidden">
-              {/* Simple line chart wireframe */}
-              <div className="flex flex-col h-full">
-                <div className="flex-1 relative pl-10">
-                  <div className="border-l-2 border-b-2 border-gray-800 h-full relative">
-                    {/* Y-axis labels */}
-                    <div className="absolute -left-10 top-0 text-gray-600">200</div>
-                    <div className="absolute -left-10 top-1/2 text-gray-600">150</div>
-                    <div className="absolute -left-10 bottom-0 text-gray-600">100</div>
-                    
-                    {/* Simulated line */}
-                    <svg className="w-full h-full" viewBox="0 0 100 100" preserveAspectRatio="none">
-                      <polyline
-                        points="2,80 20,70 40,65 60,60 80,55 98,50"
-                        fill="none"
-                        stroke="currentColor"
-                        strokeWidth="1.5"
-                        vectorEffect="non-scaling-stroke"
-                        className="text-gray-800"
-                      />
-                    </svg>
-                  </div>
-                </div>
-                {/* X-axis labels */}
-                <div className="flex justify-between mt-2 text-gray-600 pl-10">
-                  <span>Week 1</span>
-                  <span>Week 2</span>
-                  <span>Week 3</span>
-                  <span>Week 4</span>
-                </div>
-              </div>
-            </div>
-            <p className="text-gray-600 text-center mt-4">Total Weight Lifted (lbs)</p>
-          </CardContent>
-        </Card>
 
         {/* API Test Component */}
         <ApiTestComponent />
